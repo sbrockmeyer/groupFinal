7783db9bdef37e2bee972e6f0ed2880745da4286
sbrockmeyer <79857788+sbrockmeyer@users.noreply.github.com>
2022-11-21 12:07:53 -0700

    database

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+//  connection to database
+//  both ways should work
+// mongodb+srv://username:JHqkre2hvS4ZtfO8@monsterhunter.eg1mrlr.mongodb.net/?retryWrites=true&w=majority
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://username:JHqkre2hvS4ZtfO8@monsterhunter.eg1mrlr.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
