b1bd1a2e11f31bbf12e742324aa9e5fff0b5472c
sobrien-eng <sobrien@student.neumont.edu>
2022-11-30 18:34:53 -0700

    search

--- MHW Website/script/items.js
+++ MHW Website/script/items.js
@@ -2,6 +2,7 @@ let url = `http://localhost:4000/items`;
 
 let data;
 let list = document.getElementById("itemList");
+let searchTerm = document.getElementById("searchTerm");
 
 fetch(url)
     .then(response => response.json())
@@ -18,26 +19,28 @@ const main1 = () =>{
 let className = '"itemName"';
 let classRarity = '"itemRarity"';
 let classValue = '"itemValue"';
+let classCarryLimit = '"itemCarryLimit"';
 let classDescription = '"itemDescription"';
 
+function search(){
+    //findByRarity or searchTerm for any
+}
 function buildItemList(data)
 {
     ul = document.createElement('ul');
     document.getElementById('itemList').appendChild(ul);
 
-    // data.forEach(item => {
-    //     let li = document.createElement('li');
-    //     li.innerHTML += data[item].name; //+ " " + data[I].type + " " + data[I].species + " " + data[I].description;
-    //     ul.appendChild(li);
-    // });
+    //add carryLimit label
     let li = document.createElement('li');
-    li.innerHTML += "<h2 class =" + className + ">" + "Name" + "</h2>" + 
+    li.innerHTML += 
+    `<h2 class =` + className + `Name` + `</h2>` + 
     "<h2 class =" + classRarity + ">" + "Rarity" + "</h2>" +
     "<h2 class =" + classValue + ">" + "Value" + "</h2>" +
     "<h2 class =" + classDescription + ">" + "Description" + "</h2>";
     ul.appendChild(li);
 
-    for (var I = 0; I < 1186; I++)
+      //add carryLimit 
+    for (var I = 0; I < 60; I++)
     {
         let li = document.createElement('li');
         li.innerHTML += "<p class =" + className + ">" + data[I].name + "</p>" + 
